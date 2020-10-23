34b07aa18de98a883c27e9af5909919115408c74
Ngtienltmt1 <51825272+Ngtienltmt1@users.noreply.github.com>
2020-10-23 17:46:23 +0700

    add p7

---
+++ p7/index.js
@@ -0,0 +1,92 @@
+//khai bao  model
+const BlogPost = require('./models/BlogPost.js')
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true })
+
+
+//khai bao express
+//khai bao app trong express
+//khai bao path
+const express = require('express')
+const app = new express()
+const path = require('path')
+
+//khaibao fileupload
+const fileupload = require('express-fileupload')
+app.use(fileUpload())
+
+//khai bao ejs
+const ejs = require('ejs')
+app.set('view engine', 'ejs')
+
+//khai bao body-parser
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+//Dang ky thu muc public 
+app.use(express.static('public'))
+
+//Khoi tao sever
+app.listen(4000, () =>
+{
+    console.log('App listening on a port 4000')
+})
+
+
+
+//Tao router xu ly EJs
+// app.get('/',(req,res) =>{
+//     res.render('index')
+// })
+//Sua lai 
+app.get('/',(req,res) =>{
+    BlogPost.find({}, function(error, post){
+            console.log(post);
+            res.render('index',{
+                blogpost: post
+            })
+    })
+})
+
+
+
+app.get('/about',(req,res)=> {
+    res.render('about')
+})
+
+app.get('/contact',(req,res)=>{
+    res.render('contact')
+})
+
+
+//Dang ky router(tao post moi)
+app.get('/post/new', (req,res) => {
+    res.render('create')
+})              
+
+app.post('/posts/new', (req, res) => {
+    let image = req.files.image;
+    image.mv(path.resolve(__dirname, 'public/upload', image.name), function (err) {
+    // model creates a new doc with browser data
+    BlogPost.create(req.body, (error, blogpost) => {
+    res.redirect('/')
+    })
+    })
+   })
+   
+
+//     app.get('/post',(req,res)=>{
+//     res.render('post')
+// })
+
+//Sua file
+    app.get('/post/:id',(req, res) => {
+        BlogPost.findById(req.params.id, function(error,detailpost){
+            res.render('post',{
+                detailpost
+            })
+        })
+    })
